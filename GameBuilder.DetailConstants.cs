cb821f017b3c5979bfcdecb5280a31fa0608d75c
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-04 01:04:06 -0500

    Add detail pass material constants

---
+++ GameBuilder.DetailConstants.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+public partial class GameBuilder
+{
+    private static readonly Color Stone = new(0.30f, 0.295f, 0.285f);
+    private static readonly Color StoneDark = new(0.16f, 0.17f, 0.17f);
+    private static readonly Color Leather = new(0.14f, 0.050f, 0.020f);
+}
